a660b2fd86b50983994de837d21e474af2299ad9
narapady <chhuoynarapady@yahoo.com>
2022-08-10 15:36:26 -0700

    add load

--- dags/load/s3_snowflake_integration.sql
+++ dags/load/s3_snowflake_integration.sql
@@ -1,3 +1,10 @@
+
+-- Stage in snowflake represents the data sources from both
+-- internal and external of Snowflake. In this case, we use external
+-- data storage source, S3. After stage has been created we can load
+-- data into snowflake tables.
+
+-- Establish Integration between Amazon S3 and Snowflake
 create or replace storage integration s3_integration
   type = external_stage
   storage_provider = s3
@@ -6,7 +13,8 @@ create or replace storage integration s3_integration
   storage_allowed_locations = ('s3://s3-bucket-staged/', 's3://s3-bucket-staged/');
   
 DESC INTEGRATION s3_integration;
-  
+
+-- Create file format for the stage
 create or replace file format csv_format
     type = 'CSV'
     record_delimiter = "\n"
